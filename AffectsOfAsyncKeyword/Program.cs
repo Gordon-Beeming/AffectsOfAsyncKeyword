e0adf2db91803ea47d387afdae72d4dbb3251e94
Gordon Beeming <gordon.beeming@nologostudios.com>
2019-07-24 09:52:29 +0200

    Add project files.

---
+++ AffectsOfAsyncKeyword/Program.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace AffectsOfAsyncKeyword
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Tests>();
+
+        }
+        public class Tests
+        {
+            [Benchmark]
+            public void NormalMethodTest()
+            {
+                NormalMethod();
+            }
+            [Benchmark]
+            public void NormalMethodAsyncTest()
+            {
+                NormalMethodAsync();
+            }
+
+            public void NormalMethod()
+            {
+            }
+            public async void NormalMethodAsync()
+            {
+            }
+
+        }
+    }
+}
